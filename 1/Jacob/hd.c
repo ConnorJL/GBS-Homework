ef7686ce0cdcce28fed39acf5a9f3091417bc184
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2018-10-28 09:22:48 +0100

    update: use stdout instead of FILE

--- 1/Jacob/hd.c
+++ 1/Jacob/hd.c
@@ -13,26 +13,9 @@ void hexdump(FILE *output, char *buffer, int length);
 
 int main(int argc, char **argv) {
 
-	char tempc;
-	FILE *output = fopen("hexdump.txt", "w");
-	FILE *output2;
-
 	//Generate Hexdump for each argv
 	for (int i = 1; i < argc; i++) {
-		hexdump(output, argv[i], strlen(argv[i]));
+		hexdump(stdout, argv[i], strlen(argv[i]));
 	}
-	//save Hexdump to hexdump.txt
-	fclose(output);
-
-	//Print Hexdump to console
-	output2 = fopen("hexdump.txt", "r");
-
-	tempc = fgetc(output2);
-	while(tempc != EOF){
-		printf("%c", tempc);
-		tempc = fgetc(output2);
-	}
-	fclose(output2);
-
 	return 0;
 }
