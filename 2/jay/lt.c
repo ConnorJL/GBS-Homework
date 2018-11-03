34ab0fe199592d43322c253135a15dbb9a730951
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2018-11-03 20:49:09 +0100

    no message

---
+++ 2/jay/lt.c
@@ -0,0 +1,77 @@
+/*
+ * lt.c
+ *
+ *  Created on: 03.11.2018
+ *      Author: jobrm
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "list.h"
+
+int main(int argc, char **argv) {
+
+	list_t *li;
+	struct list_elem *li_el;
+	if((li = list_init()) == NULL){
+		perror("Cannot allocate memory");
+		exit(-1);
+	}
+
+
+	int i = 1;
+	char *cmd = argv[i];
+	char *args = argv[i+1];
+
+	while(i <= argc && cmd != NULL && args != NULL){
+
+		if(cmd[i] != '-')
+			break;
+
+		switch(cmd[i+1]){
+			case 'a':
+				list_append(li, args);
+				break;
+			case 'i':
+				list_insert(li, args);
+				break;
+			case 'r':
+				li_el = list_find(li,args,strcmp);
+				list_remove(li, li_el);
+				break;
+		}
+		i += 2;
+		cmd = argv[i];
+		args = argv[i+1];
+	}
+
+	list_print(li, print_string);
+
+
+
+
+
+//	list_t *li;
+//
+//	if((li = list_init()) == NULL){
+//		perror("Cannot allocate memory");
+//		exit(-1);
+//	}
+//	if(list_append(li, "erstes") == NULL ||
+//		list_append(li, "zweites") == NULL ||
+//		list_append(li, "drittes") == NULL ){
+//		perror("Cannot allocate memory");
+//		exit(-1);
+//	}
+//	if(list_insert(li, "erstes") == NULL ||
+//		list_insert(li, "zweites") == NULL ||
+//		list_insert(li, "drittes") == NULL ){
+//		perror("Cannot allocate memory");
+//		exit(-1);
+//	}
+//	list_print(li, print_string);
+//	list_finit(li);
+//	list_print(li, print_string);
+//	return 1;
+}
+
