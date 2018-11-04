b0fddfe74a27e1ff640d2048ec1d2f4e3ae0e285
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2018-11-04 12:26:05 +0100

    shellsim test passed

--- 2/jay/shellsim.c
+++ 2/jay/shellsim.c
@@ -15,7 +15,13 @@ int main(int argc, char **argv) {
 	char checki;
 
 	while(1){
+
+		printf("bash: ");
 		checkc = fgets(cmd, 256, stdin);
+
+	    if(cmd[strlen(cmd) - 1] == '\n'){
+	      cmd[strlen(cmd) - 1] = '\0';
+	    }
 
 		if(strcmp(checkc, "s") == 0 || strncmp(cmd, "exit", 4) == 0){
 			break;
@@ -28,4 +34,5 @@ int main(int argc, char **argv) {
 		}
 
 	}
+	return 1;
 }
