caf473b3cdee3a45f141817f3626ef95f45a8146
student <student@gbsvm>
2018-11-04 18:40:20 +0100

    Fixed syntax and warnings

--- 2/connor/shellsim.c
+++ 2/connor/shellsim.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]) {
     char s[256];
     while(1) {
-        if(fgets(s, 255, stdin) == NULL {
+        if(fgets(s, 255, stdin) == NULL) {
             return -1;
         }
         if(strncmp(s,"exit", 4) == 0) {
@@ -14,3 +16,4 @@ main(int argc, char const *argv[]) {
         }
     }
 }
+
