cf9e76fa74cefc947966b17c65c0915be1fdf476
student <student@gbsvm>
2018-11-04 18:16:05 +0100

    Added

--- 2/connor/shellsim.c
+++ 2/connor/shellsim.c
@@ -3,7 +3,9 @@
 main(int argc, char const *argv[]) {
     char s[256];
     while(1) {
-        fgets(s, 255, stdin);
+        if(fgets(s, 255, stdin) == NULL {
+            return -1;
+        }
         if(strncmp(s,"exit", 4) == 0) {
             return 0;
         }
