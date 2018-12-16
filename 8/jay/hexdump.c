c0128a6387e9e05fa182f40ccc00bb0f5c8ec0ed
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2018-12-16 22:58:45 +0100

    echo/hexdump server current moodle status

---
+++ 8/jay/hexdump.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void hexdump(int sd, struct sockaddr *sin, int sender_len, char *buffer, int length) {
+    int count = 0;
+    int done = 0;
+    char buf[1024*80];
+    buf[0] = '\0';
+    //printf("Test: %.2x\n", buffer[0]);
+    // Looping over lines
+    while(done == 0) {
+        sprintf(buf, "%s%.6x : ", buf, count);
+        for(int i = 0; i<16; i++) {
+            if(done == 0) {
+                sprintf(buf, "%s%.2x ", buf, (unsigned char) buffer[count]);
+            }
+            else {
+                sprintf(buf, "%s   ", buf);
+            }
+            if(count == length) {
+                done = 1;
+            }
+            count++;
+        }
+        count = count - 16;
+        sprintf(buf, "%s  ", buf);
+        for(int i = 0; i<16; i++) {
+            if(count == length) {
+                done = 2;
+            }
+            if(buffer[count] >= 0x20 && buffer[count] <=0x7e) {
+                sprintf(buf, "%s%c", buf, buffer[count]);
+            }
+            else {
+                sprintf(buf, "%s.", buf);
+            }
+            if(done == 2) {
+                break;
+            }
+            count++;
+        }
+        sprintf(buf, "%s\n", buf);
+
+
+//        write(socketfd, buf, sizeof(buf));
+    }
+
+    int send_stat = sendto(sd, buf, strlen(buf), 0, sin,
+                      sender_len);
+    if (send_stat < 0) {
+        perror("write() to socket Error\n");;
+        exit(-1);
+    }
+}
+
+//void hexdump(FILE *buf, char *buffer, int length) {
+//    int count = 0;
+//    int done = 0;
+//    //printf("Test: %.2x\n", buffer[0]);
+//    // Looping over lines
+//    while(done == 0) {
+//        fprintf(buf, "%.6x : ", count);
+//        for(int i = 0; i<16; i++) {
+//            if(done == 0) {
+//                fprintf(buf, "%.2x ", (unsigned char) buffer[count]);
+//            }
+//            else {
+//                fprintf(buf, "   ");
+//            }
+//            if(count == length) {
+//                done = 1;
+//            }
+//            count++;
+//        }
+//        count = count - 16;
+//        fprintf(buf, "  ");
+//        for(int i = 0; i<16; i++) {
+//            if(count == length) {
+//                done = 2;
+//            }
+//            if(buffer[count] >= 0x20 && buffer[count] <=0x7e) {
+//                fprintf(buf, "%c", buffer[count]);
+//            }
+//            else {
+//                fprintf(buf, ".");
+//            }
+//            if(done == 2) {
+//                break;
+//            }
+//            count++;
+//        }
+//        fprintf(buf, "\n");
+//    }
+//}
--- 8/jay/server_v3.0/server.c
+++ 8/jay/server_v3.0/server.c
@@ -10,7 +10,6 @@
 #include <sys/types.h>
 #include <sys/un.h>
 #include <sys/time.h>
-#include <sys/wait.h>
 #include <netdb.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -20,7 +19,6 @@
 #include <errno.h>
 
 #define PORT 44445
-#define HDPORT 44446
 #define SIZE 1024
 #define MAX_CONNECTIONS 10
 #define SERVER_TIMEOUT 10000 	//10 seconds
@@ -84,39 +82,6 @@ int is_timedout(struct connection conn) {
 	return check;
 }
 
-void hexdump(int sd, struct sockaddr *sin, int sender_len, char *buffer,
-		int length);
-
-void hexdumpservice(struct connection conn) {
-	if (conn.sd < 0)
-		return;
-
-	char input[SIZE];
-	int sizeread;
-	socklen_t sender_len = sizeof(conn.sin);
-
-	bzero(input, SIZE);
-
-	sizeread = recvfrom(conn.sd, input, sizeof(input), 0,
-			(struct sockaddr *) &conn.sin, &sender_len);
-	if (sizeread < 0)
-		handle_error("read() from socket Error\n");
-	if (sizeread >= 1024) {
-		input[1023] = '\0';
-		sizeread = 1023;
-	}
-
-	input[sizeread] = '\0';
-
-	hexdump(conn.sd, (struct sockaddr *) &conn.sin, sender_len, input,
-			sizeread - 1);
-
-//    sizeread = sendto(conn.sd, input, sizeread, 0, (struct sockaddr *) &conn.sin,
-//                      sender_len);
-//    if (sizeread < 0)
-//        handle_error("write() to socket Error\n");;
-}
-
 int main(int argc, char **argv) {
 
 	int socketfd, remote_socketfd, check;
@@ -125,140 +90,6 @@ int main(int argc, char **argv) {
 	struct connection conn_logs[MAX_CONNECTIONS];
 	int server_shutdown = 0, client_shutdown = 0;
 	int num_of_pfds = 1, num_of_active_sockets;
-
-	int process = fork();
-
-	if (process == 0) {
-		printf("launching hexdump server\n");
-
-		socketfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-		check = setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, // | SO_LINGER,
-				(char *) &num_of_pfds, sizeof(num_of_pfds));
-		if (socketfd == -1 || check == -1)
-			handle_error(" socket() Error\n");
-		else
-			printf(" success socket()\n");
-
-		bzero(&server_addr, sizeof(server_addr)); //Clear
-
-		server_addr.sin_family = AF_INET;
-		server_addr.sin_port = htons(HDPORT);
-		server_addr.sin_addr.s_addr = INADDR_ANY;
-
-		check = bind(socketfd, (struct sockaddr *) &server_addr,
-				sizeof(server_addr));
-		if (check != 0) {
-			close(socketfd);
-			handle_error(" bind() Error\n");
-		} else
-			printf(" success bind()\n");
-
-		check = listen(socketfd, 10);
-		if (check != 0) {
-			close(socketfd);
-			handle_error(" listen() Error\n");
-		} else
-			printf(" success listen()\n");
-
-		//set listening fd
-		pfds[0].fd = socketfd;
-		pfds[0].events = POLLIN;
-
-		//Initialize poll fd's
-		for (int i = 1; i < MAX_CONNECTIONS; i++)
-			pfds[i].fd = -1;
-
-		check = poll(pfds, num_of_pfds, SERVER_TIMEOUT * 3);
-		if (check < 0)
-			printf("  hexdump poll() Error\n");
-		 else if (check == 0)
-			printf("  hexdump poll() timed out, shutting down...\n");
-
-
-		while (1) {
-
-			if (server_shutdown)
-				break;
-
-			printf(" Starting poll()\n");
-			check = poll(pfds, num_of_pfds, SERVER_TIMEOUT);
-			if (check < 0) {
-				printf("  hexdump poll() Error\n");
-				break;
-			} else if (check == 0) {
-				printf("  hexdump poll() timed out, shutting down...\n");
-				break;
-			}
-
-			printf(" Managing active sockets\n");
-			num_of_active_sockets = num_of_pfds;
-			for (int i = 0; i < num_of_active_sockets; i++) {
-
-				if (pfds[i].revents == 0)
-					if (pfds[i].fd != socketfd && is_timedout(conn_logs[i])) {
-						printf("   a client timed out\n");
-						client_shutdown = 1;
-						close(pfds[i].fd);
-						pfds[i].fd = -1;
-					} else
-						continue; //nothing happed, skip
-				else { //Adding new connection
-					if (pfds[i].fd == socketfd) {
-						printf("   processing server socket access request\n");
-
-						while (1) {
-							remote_socketfd = accept(socketfd, NULL, NULL);	//(struct sockaddr *) &peer_addr,	&peer_addr_size);
-							if (remote_socketfd < 0) {
-								if (errno != EWOULDBLOCK) {
-									perror("   accept() Error, shutting down...\n");
-									server_shutdown = 1;
-								}
-								break;
-							}
-							gettimeofday(&conn_logs[num_of_pfds].timeout, NULL);
-							conn_logs[num_of_pfds].sd = remote_socketfd;
-							pfds[num_of_pfds].fd = remote_socketfd;
-							pfds[num_of_pfds].events = POLLIN;
-							num_of_pfds++;
-
-							printf("   added new remote socket\n");
-						}
-					} else { //do echo service
-						printf("   processing remote socket access request\n");
-						gettimeofday(&conn_logs[i].timeout, NULL); //reset timeout
-						hexdumpservice(conn_logs[i]);
-					}
-				}
-			}
-
-			//move active clients to start of the poll file descriptors
-			if (client_shutdown) {
-				client_shutdown = 0;
-				for (int i = 0; i < num_of_pfds; i++) {
-					if (pfds[i].fd == -1) {
-						for (int k = i; k < num_of_pfds; k++) {
-							pfds[k] = pfds[k + 1];
-							conn_logs[k] = conn_logs[k + 1];
-						}
-						i--;
-						num_of_pfds--;
-					}
-				}
-			}
-		}
-
-		printf(" closing open file descriptors\n");
-		for (int i = 0; i < num_of_pfds; i++)
-			if (pfds[i].fd >= 0) {
-				check = close(pfds[i].fd);
-				if (check < 0)
-					handle_error("close() Error\n");
-			}
-
-		printf(" success hexdumpserver close()\n");
-
-		return 1;
-	}
 
 	printf("launching echo server\n");
 
@@ -306,10 +137,10 @@ int main(int argc, char **argv) {
 		printf(" Starting poll()\n");
 		check = poll(pfds, num_of_pfds, SERVER_TIMEOUT);
 		if (check < 0) {
-			printf("  echo poll() Error\n");
+			printf("  poll() Error\n");
 			break;
 		} else if (check == 0) {
-			printf("  echo poll() timed out, shutting down...\n");
+			printf("  poll() timed out, shutting down...\n");
 			break;
 		}
 
@@ -378,10 +209,7 @@ int main(int argc, char **argv) {
 				handle_error("close() Error\n");
 		}
 
-	printf(" success echoserver close()\n");
-	wait(NULL);
-
-	printf("success server close()\n");
+	printf("success close()\n");
 
 	return 1;
 }
